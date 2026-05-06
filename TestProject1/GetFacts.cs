c6834f8d7d6248de5fd764e9e6b16f275802aff6
Khen <kalbarico91@rouse.com>
2026-05-06 14:17:09 +0800

    Test operations commented out1

--- TestProject1/GetFacts.cs
+++ TestProject1/GetFacts.cs
@@ -1,30 +1,30 @@
-﻿using ProjectManagement1.Core.Repository;
-using ProjectManagement1.Models;
-using TestProject1.TestTools;
-using Xunit.Abstractions;
+﻿//using ProjectManagement1.Core.Repository;
+//using ProjectManagement1.Models;
+//using TestProject1.TestTools;
+//using Xunit.Abstractions;
 
-namespace TestProject1;
+//namespace TestProject1;
 
-public class GetFacts (ITestOutputHelper _ctx)
-{
-    [Fact] public void Load_Projects()
-    {
-        var _sut = _ctx.Get<IAppRepository>();
+//public class GetFacts (ITestOutputHelper _ctx)
+//{
+//    [Fact] public void Load_Projects()
+//    {
+//        var _sut = _ctx.Get<IAppRepository>();
 
-        var res = _sut.LoadDatasource<List<Project>>();
-    }
+//        var res = _sut.LoadDatasource<List<Project>>();
+//    }
 
-    [Fact] public void Load_Developers()
-    {
-        var _sut = _ctx.Get<IAppRepository>();
+//    [Fact] public void Load_Developers()
+//    {
+//        var _sut = _ctx.Get<IAppRepository>();
 
-        var res = _sut.LoadDatasource<List<Developer>>();
-    }
+//        var res = _sut.LoadDatasource<List<Developer>>();
+//    }
 
-    [Fact] public void Load_WorkItems()
-    {
-        var _sut = _ctx.Get<IAppRepository>();
+//    [Fact] public void Load_WorkItems()
+//    {
+//        var _sut = _ctx.Get<IAppRepository>();
 
-        var res = _sut.LoadDatasource<List<WorkItem>>();
-    }
-}
+//        var res = _sut.LoadDatasource<List<WorkItem>>();
+//    }
+//}
--- TestProject1/PostFacts.cs
+++ TestProject1/PostFacts.cs
@@ -1,61 +1,61 @@
-﻿using ProjectManagement1.Core.Repository;
-using ProjectManagement1.Core.Utilities;
-using ProjectManagement1.Models;
-using ProjectManagement1.Models.Constants;
-using ProjectManagement1.Models.Enums;
-using TestProject1.TestTools;
-using Xunit.Abstractions;
+﻿//using ProjectManagement1.Core.Repository;
+//using ProjectManagement1.Core.Utilities;
+//using ProjectManagement1.Models;
+//using ProjectManagement1.Models.Constants;
+//using ProjectManagement1.Models.Enums;
+//using TestProject1.TestTools;
+//using Xunit.Abstractions;
 
-namespace TestProject1;
+//namespace TestProject1;
 
-public class PostFacts (ITestOutputHelper _ctx)
-{
-    [Fact] public void Post_Project()
-    {
-        var payload = new Project()
-        {
-            Uid       = Prefixes.ProjectPrefix.Generate(),
-            Title     = "Conflict Check",
-            Priority  = ProjectPriorityStatus.NotPriority,
-            Activity  = ProjectActivityStatus.LowActivity,
-            KeyPerson = KeyPersons.Liza
+//public class PostFacts (ITestOutputHelper _ctx)
+//{
+//    [Fact] public void Post_Project()
+//    {
+//        var payload = new Project()
+//        {
+//            Uid       = Prefixes.ProjectPrefix.Generate(),
+//            Title     = "Conflict Check",
+//            Priority  = ProjectPriorityStatus.NotPriority,
+//            Activity  = ProjectActivityStatus.LowActivity,
+//            KeyPerson = KeyPersons.Liza
 
-        };
+//        };
 
-        var _sut = _ctx.Get<IAppRepository>();
+//        var _sut = _ctx.Get<IAppRepository>();
 
-        _sut.PostProject(payload);
-    }
+//        _sut.PostProject(payload);
+//    }
 
-    [Fact] public void Post_Developer()
-    {
-        var payload = new Developer()
-        {
-            Uid             = Prefixes.DeveloperPrefix.Generate(),
-            Name            = "Marcus",
-            Workload        = DeveloperWorkloadStatus.Standby,
-            Health          = DeveloperHealthStatus.Healthy,
-            Project         = ["PRJ-12345"],
-            ProjectExposure = DeveloperProjectExposureStatus.UnderExposed
-        };
+//    [Fact] public void Post_Developer()
+//    {
+//        var payload = new Developer()
+//        {
+//            Uid             = Prefixes.DeveloperPrefix.Generate(),
+//            Name            = "Marcus",
+//            Workload        = DeveloperWorkloadStatus.Standby,
+//            Health          = DeveloperHealthStatus.Healthy,
+//            Project         = ["PRJ-12345"],
+//            ProjectExposure = DeveloperProjectExposureStatus.UnderExposed
+//        };
 
-        var _sut = _ctx.Get<IAppRepository>();
+//        var _sut = _ctx.Get<IAppRepository>();
 
-        _sut.PostDeveloper(payload);
-    }
+//        _sut.PostDeveloper(payload);
+//    }
 
-    [Fact] public void Post_WorkItem()
-    {
-        var payload = new WorkItem()
-        {
-            Uid       = Prefixes.ProjectPrefix.Generate(),
-            Reference = "https://dev.azure.com/RouseCo/Conflict%20Check%20AI/_workitems/edit/14228",
-            Developer = "DEV-12345",
-            Status    = WorkItemStatus.Investigating
-        };
+//    [Fact] public void Post_WorkItem()
+//    {
+//        var payload = new WorkItem()
+//        {
+//            Uid       = Prefixes.ProjectPrefix.Generate(),
+//            Reference = "https://dev.azure.com/RouseCo/Conflict%20Check%20AI/_workitems/edit/14228",
+//            Developer = "DEV-12345",
+//            Status    = WorkItemStatus.Investigating
+//        };
 
-        var _sut = _ctx.Get<IAppRepository>();
+//        var _sut = _ctx.Get<IAppRepository>();
 
-        _sut.PostWorkItem(payload);
-    }
-}
+//        _sut.PostWorkItem(payload);
+//    }
+//}
